674dbc4ab0b091418ba100c8cb2c03c096ad2d45
Posadowski <mateuszposa@gmail.com>
2024-06-15 18:21:59 +0200

    feat: add SFML libs to Makefile

--- inc/gameMachinery.h
+++ inc/gameMachinery.h
@@ -3,11 +3,17 @@
 
 #include <iostream>
 #include <vector>
+#include <SFML/Graphics.hpp>
 
 #define COLUMNS 3
 #define ROWS 3
 
-void drawMap(const std::vector<std::vector<char>> &currentBoard);
+#define WINDOW_WIDTH  600
+#define WINDOW_HEIGHT 600
+#define CELL_SIZE 200
+#define PADDING 5
+
+void drawBoard(sf::RenderWindow& window, std::vector<std::vector<char>>& board,sf::Font& font);
 bool userSelectedTeamCorrectly(char *userInput);
 bool userSelectedRowOrColumnCorrectly(char userInput);
 bool checkIfGameFinished(const std::vector<std::vector<char>> &currentBoard, char currentPlayer);
